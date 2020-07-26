b7654d68ac6b548422896b8324d85fe4a4226ae6
sadopsa <ahmadilham48oo@gmail.com>
2020-07-26 15:40:18 +0700

    solusiDetail.php modify

--- topik/solusiDetail.php
+++ topik/solusiDetail.php
@@ -1,11 +1,123 @@
+<?php 
+include("../config/config.php");
+$id = $_GET['id'];
+$solusi = mysqli_query($connect,"SELECT * FROM solusi where id = $id");
+$data = mysqli_fetch_array($solusi);
+$i = 1;
+$kolom = 3;
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-    <h1>solusi</h1>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="">
+    <div class="container">
+
+      <div class="logo float-left">
+        
+          <a href="../index.php">
+            <a href="../index.php">
+              <img src="../assets/img/logo.png" alt="" class="img-fluid" height="50" width="200">
+            </a></div>
+
+            
+     <!------ <div class="logo float-right">
+        <h1 class="text-light">
+         <span>Digital Solution</span></a></h1></div>---->
+      <!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  <main id="mains">
+
+    <!-- ======= Services Section ======= -->
+    <!-- End Services Section -->
+  
+    <!-- ======= Why Us Section ======= -->
+    <section class="why-us section-bg" data-aos="fade-up" date-aos-delay="200">  
+    <h1 class="tepi-sikit" style="text-align:left; padding-left: 2%; text-transform:uppercase;"><?php echo $data['judul']; ?></h1>
+    <div class="containers" >
+        <div class="row">
+        
+         
+       
+      
+          
+<!----
+<div class="icon-box">
+              <div class="icon"><i class="bx bx-gift"></i></div>
+              <h4 class="title"><a href="">SEMV</a></h4>
+              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+            </div>--->
+   
+          </div>
+        </div>
+
+      </div>
+     
+     
+    </section><!-- End Why Us Section -->
+     
+
+    <!-- ======= Features Section ======= -->
+   
+  <!-- End Features Section -->
+
+  </main><!-- End #main -->
+ 
+ <!-- ======= Footer ======= -->
+ <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+
+
+</footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/jquery/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/venobox/venobox.min.js"></script>
+  <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="../assets/vendor/counterup/counterup.min.js"></script>
+  <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+<!-- REQUIRED SCRIPTS -->
+<?php include("../partial/js.php"); ?>
 </body>
 </html>
