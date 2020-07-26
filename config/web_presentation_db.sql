71ab49537089ea53d917c27e1fd5baf243b043da
sadopsa <ahmadilham48oo@gmail.com>
2020-07-26 15:39:35 +0700

    db sql modify

--- config/web_presentation_db.sql
+++ config/web_presentation_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2020 at 09:04 AM
+-- Generation Time: Jul 26, 2020 at 10:27 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -24,6 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `solusi`
+--
+
+CREATE TABLE `solusi` (
+  `id` int(11) NOT NULL,
+  `idtopikdetail` int(3) NOT NULL,
+  `judul` varchar(10) NOT NULL,
+  `sebelum` varchar(30) NOT NULL,
+  `sesudah` varchar(30) NOT NULL,
+  `keybenefit` varchar(30) NOT NULL,
+  `materi` varchar(30) NOT NULL,
+  `process` varchar(30) NOT NULL,
+  `video` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `solusi`
+--
+
+INSERT INTO `solusi` (`id`, `idtopikdetail`, `judul`, `sebelum`, `sesudah`, `keybenefit`, `materi`, `process`, `video`) VALUES
+(1, 1, 'solusi1_1', '', '', '', '', '', ''),
+(2, 2, 'solusi2_1', '', '', '', '', '', ''),
+(3, 3, 'solusi1_2', '', '', '', '', '', ''),
+(4, 4, 'solusi1_3', '', '', '', '', '', ''),
+(5, 5, 'solusi2_2', '', '', '', '', '', ''),
+(6, 6, 'solusi2_3', '', '', '', '', '', ''),
+(7, 7, 'solusi2_4', '', '', '', '', '', ''),
+(8, 8, 'solusi2_5', '', '', '', '', '', ''),
+(9, 9, 'solusi2_6', '', '', '', '', '', ''),
+(10, 10, 'solusi2_7', '', '', '', '', '', ''),
+(11, 11, 'solusi2_8', '', '', '', '', '', ''),
+(12, 12, 'solusi2_9', '', '', '', '', '', ''),
+(13, 13, 'solusi2_10', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `topik`
 --
 
@@ -39,8 +76,8 @@ CREATE TABLE `topik` (
 --
 
 INSERT INTO `topik` (`id`, `judul`, `deskripsi`, `image`) VALUES
-(1, 'JAVIL', 'Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident', 'javil.jpg'),
-(2, 'SEMV', 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque', 'semv.jpg');
+(1, 'JAVIL', 'Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident', 'javil.gif'),
+(2, 'SEMV', 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque', 'semv.gif');
 
 -- --------------------------------------------------------
 
@@ -61,11 +98,30 @@ CREATE TABLE `topik_detail` (
 
 INSERT INTO `topik_detail` (`id`, `idtopik`, `judul`, `image`) VALUES
 (1, 1, 'solusi1_1', 'sol11.jpg'),
-(2, 2, 'solusi2_1', 'sol21.jpg');
+(2, 2, 'solusi2_1', 'sol21.jpg'),
+(3, 1, 'solusi1_2', 'sol12.jpg'),
+(4, 1, 'solusi1_3', 'sol13.jpg'),
+(5, 2, 'solusi2_2', 'sol22.jpg'),
+(6, 2, 'solusi2_3', 'sol23.jpg'),
+(7, 2, 'solusi2_4', 'sol24.jpg'),
+(8, 2, 'solusi2_5', 'sol25.jpg'),
+(9, 2, 'solusi2_6', 'sol26.jpg'),
+(10, 2, 'solusi2_7', 'sol27.jpg'),
+(11, 2, 'solusi2_8', 'sol28.jpg'),
+(12, 2, 'solusi2_9', 'sol29.jpg'),
+(13, 2, 'solusi2_10', 'sol210.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `solusi`
+--
+ALTER TABLE `solusi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idtopikdetail` (`idtopikdetail`),
+  ADD KEY `judul` (`judul`);
 
 --
 -- Indexes for table `topik`
@@ -78,11 +134,18 @@ ALTER TABLE `topik`
 --
 ALTER TABLE `topik_detail`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `judultopik` (`judul`),
   ADD KEY `topikid` (`idtopik`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `solusi`
+--
+ALTER TABLE `solusi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `topik`
@@ -94,11 +157,18 @@ ALTER TABLE `topik`
 -- AUTO_INCREMENT for table `topik_detail`
 --
 ALTER TABLE `topik_detail`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `solusi`
+--
+ALTER TABLE `solusi`
+  ADD CONSTRAINT `solusi_ibfk_1` FOREIGN KEY (`idtopikdetail`) REFERENCES `topik_detail` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `solusi_ibfk_2` FOREIGN KEY (`judul`) REFERENCES `topik_detail` (`judul`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `topik_detail`
